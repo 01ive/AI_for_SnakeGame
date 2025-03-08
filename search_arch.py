291bde9f415ab2684a0c3c60b67360cc00665c85
olive <01ive@free.fr>
2025-03-08 08:09:39 +0100

    Improve search arch

--- search_arch.py
+++ search_arch.py
@@ -2,6 +2,7 @@
 import os
 from datetime import datetime as dt
 import logging
+import json
 import pickle
 # Module imports
 import plotly.graph_objects as go
@@ -10,46 +11,59 @@ from snake_api import SnakeApi
 # AI imports
 from ai_agent import Agent, LinearQNet, DeepQNet
 
-DATE = str(dt.now().year) + str(dt.now().month) + str(dt.now().day) +  str(dt.now().hour) + str(dt.now().minute) + str(dt.now().second)
+from main import save_data
 
 
 if __name__ == '__main__':
+    DATE = str(dt.now().year) + str(dt.now().month) + str(dt.now().day) + '_' + str(dt.now().hour) + str(dt.now().minute) + str(dt.now().second)
+
+    rewards = [ {
+                    'collision': -10,
+                    'self collision': -10,
+                    'food': 10,
+                    'closer_to_food': 5 
+                }, {
+                    'collision': -10,
+                    'self collision': -10,
+                    'food': 10,
+                    'closer_to_food': 2
+                }, {
+                    'collision': -20,
+                    'self collision': -20,
+                    'food': 50,
+                    'closer_to_food': 5 
+                } ]
+
+    inference = False
+    nb_game = 500
+    game_speed = 10000
+
     out_dir = DATE
     os.mkdir(out_dir)
 
     logging.basicConfig(filename=os.path.join(out_dir, 'log_' + DATE + '.log'), encoding='utf-8', level=logging.DEBUG)
-    logging.getLogger().addHandler(logging.StreamHandler())
-    
-    inference = False
-    nb_game = 10
-    game_speed = 10000
+    logging.getLogger().addHandler(logging.StreamHandler())  
 
     scores_by_model_architecture = {}
 
     game = SnakeApi()
+    
+    with open(os.path.join(out_dir, 'rewards.json'), 'w') as f:
+        json.dump(rewards, f, indent=2)
 
     for network_type in [LinearQNet, DeepQNet]:
         for hidden_layer_size in [64, 128, 256, 512, 1024]:
-            # Train architecture
-            architecture_name = network_type.__name__ + '_' + str(hidden_layer_size)
-            logging.info('Training ' + architecture_name)
-            agent = Agent(game, inference, nb_game, hidden_layer_size, network_type, os.path.join(out_dir, architecture_name + '.pth'))
-            agent.train(game_speed)
-            # Save the scores
-            if inference:
-                data_file = os.path.join(out_dir, architecture_name + '_inference.pkl')
-            else:
-                data_file = os.path.join(out_dir, architecture_name + '_train.pkl')
-            if os.path.exists(data_file):
-                with open(data_file, 'rb') as f:
-                    all_scores = pickle.load(f)
-            else:
-                all_scores = []
-            all_scores.extend(agent.scores)
-            with open(data_file, 'wb') as f:
-                pickle.dump(all_scores, f)
-            # Save model architecture score
-            scores_by_model_architecture[architecture_name] = all_scores
+            for n, reward in enumerate(rewards):
+                game.rewards = reward
+                # Train architecture
+                architecture_name = network_type.__name__ + '_' + str(hidden_layer_size) + '_' + str(n)
+                logging.info('Training ' + architecture_name)
+                agent = Agent(game, inference, nb_game, hidden_layer_size, network_type, os.path.join(out_dir, architecture_name + '.pth'))
+                agent.train(game_speed)
+                # Save the scores
+                all_scores = save_data(inference, out_dir, architecture_name, agent.scores)
+                # Save model architecture score
+                scores_by_model_architecture[architecture_name] = all_scores
     
     # Display score graph
     logging.info('Displaying score graph')
