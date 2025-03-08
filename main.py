749c49d4d0ff130ea7f1de23a672b4f7ba8ff546
olive <01ive@free.fr>
2025-03-08 08:09:39 +0100

    Improve search arch

--- main.py
+++ main.py
@@ -12,18 +12,38 @@ from snake_api import SnakeApi
 # AI imports
 from ai_agent import Agent, LinearQNet, DeepQNet
 
-DATE = str(dt.now().year) + str(dt.now().month) + str(dt.now().day) +  str(dt.now().hour) + str(dt.now().minute) + str(dt.now().second)
+
+def save_data(inference, out_dir, architecture_name, score):
+    if inference:
+        data_file = os.path.join(out_dir, architecture_name + '_inference.pkl')
+    else:
+        data_file = os.path.join(out_dir, architecture_name + '_train.pkl')
+    if os.path.exists(data_file):
+        with open(data_file, 'rb') as f:
+            all_scores = pickle.load(f)
+    else:
+        all_scores = []
+    all_scores.extend(score)
+    with open(data_file, 'wb') as f:
+        pickle.dump(all_scores, f)
+    return all_scores
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='log_' + DATE + '.log', encoding='utf-8', level=logging.DEBUG)
-    logging.getLogger().addHandler(logging.StreamHandler())
-    
+    DATE = str(dt.now().year) + str(dt.now().month) + str(dt.now().day) + '_' + str(dt.now().hour) + str(dt.now().minute) + str(dt.now().second)
+
     inference = True
     nb_game = 1
+    weight_file = 'model_weights.pth'
 
+    out_dir = DATE
+    os.mkdir(out_dir)
+
+    logging.basicConfig(filename=os.path.join(out_dir, 'log_' + DATE + '.log'), encoding='utf-8', level=logging.DEBUG)
+    logging.getLogger().addHandler(logging.StreamHandler())
+    
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"htin:",["help", "train", "inference", "nb="])
+        opts, args = getopt.getopt(sys.argv[1:],"htin:w:",["help", "train", "inference", "nb=", "weight="])
     except getopt.GetoptError:
         print ('main.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
@@ -38,30 +58,18 @@ if __name__ == '__main__':
             inference = True
         elif opt in ("-n", "--nb"):
             nb_game = int(arg)
+        elif opt in ("-w", "--weight"):
+            weight_file = int(arg)
 
     game = SnakeApi()
-    agent = Agent(game, inference, nb_game, 128, LinearQNet)
+    agent = Agent(game, inference, nb_game, 128, LinearQNet, weight_file)
     agent.train(10000)
 
     # Save the scores
-    if inference:
-        data_file = 'data_inference.pkl'
-    else:
-        data_file = 'data_train.pkl'
-    
-    if os.path.exists(data_file):
-        with open(data_file, 'rb') as f:
-            all_scores = pickle.load(f)
-    else:
-        all_scores = []
-
-    all_scores.extend(agent.scores)
-    
-    with open(data_file, 'wb') as f:
-        pickle.dump(all_scores, f)
+    all_scores = save_data(inference, out_dir, 'data', agent.scores)
     
     # Display score graph
     x = list(range(len(all_scores)))
     fig = go.Figure(data=go.Scatter(x=x, y=all_scores, mode='lines+markers'))
-    fig.write_html(DATE + '_score.html')
+    fig.write_html(os.path.join(out_dir, '_score.html'))
     fig.show()
